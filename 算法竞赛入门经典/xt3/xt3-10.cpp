d9a35a716add3253d9cf58622b86ccdfd83f5a7a
X <qq383441907@outlook.com>
2016-08-27 16:53:09 +0800

    Update xt3-10.cpp

--- 算法竞赛入门经典/xt3/xt3-10.cpp
+++ 算法竞赛入门经典/xt3/xt3-10.cpp
@@ -3,46 +3,58 @@
 #include<algorithm>
 #include<iostream>
 using namespace std;
-//wa
+struct face{
+int x;
+int y;
+}f[6];
+int cmp(face f1,face f2)
+{
+    if(f1.x!=f2.x) return f1.x>f2.x;
+    else return f1.y>f2.y;
+}
 int main()
 {
-    int width[6],height[6];
-    int i=0,j=0,w,h;
-    while(scanf("%d %d",&w,&h)!=EOF)
-    {
-        if(w>h){
-        width[i++]=w;
-        height[j++]=h;
+   int h,w;
+   while(scanf("%d %d",&w,&h)!=EOF)
+   {
+       if(w>h)
+        {
+            f[0].x=w;f[0].y=h;
         }
         else{
-            width[i++]=h;height[j++]=w;
+            f[0].x=h;f[0].y=w;
         }
-        for(;i<6;){
-            scanf("%d %d",&w,&h);
-            if(w>h){
-                     width[i++]=w;
-                     height[j++]=h;
-                    }
-            else{
-                width[i++]=h;height[j++]=w;
-                }
+       int i=1;
+       while(i<6)
+       {
+           scanf("%d %d",&w,&h);
+            if(w>h)
+            {
+                f[i].x=w;f[i].y=h;
             }
-
-        if(i==6) {
-            i=0;
-            j=0;
-            sort(width,width+6);
-            sort(height,height+6);
-            if(width[0]==width[1]&&width[2]==width[3]&&width[4]==width[5]&&
-            height[0]==height[1]&&height[2]==height[3]&&height[4]==height[5])
-                 printf("POSSIBLE\n");
-            else printf("IMPOSSIBLE\n");
-
-        }
+            else{
+            f[i].x=h;f[i].y=w;
+            }
+            i++;
+       }
+    sort(f,f+6,cmp);
+    int flag=1;
+    for(int i=0;i<6;i+=2)
+    {
+        if(f[i].x!=f[i+1].x||f[i].y!=f[i+1].y)
+            flag=0;
     }
+    if(flag==0)
+    {
+        cout<<"IMPOSSIBLE"<<endl;
+        continue;
+    }
+   // for(int i=0;i<6;i++)
+     //   cout<<f[i].x<<' '<<f[i].y<<endl;
+    if(f[1].x==f[3].x&&f[0].y==f[5].x&&f[3].y==f[5].y)//åˆ¤æ–­3ä¸ªé¢æ˜¯å¦æž„æˆç«‹æ–¹ä½“
+        cout<<"POSSIBLE"<<endl;
+	else cout<<"IMPOSSIBLE"<<endl;
+   }
 	return 0;
 }
-//uva1587
-//waÔ­Òò,Î´ÅÐ¶Ï3¸öÃæÊÇ·ñÄÜ½ÓÔÚÒ»Æð
-
-//ac´ó¸Ä£º
+//uva1587ï¼š
