dcbc73fdbbf3a56d7f4a2512deb58606b4a74698
X <qq383441907@outlook.com>
2016-11-20 22:05:25 +0800

    sb题要敢写

---
+++ codeforces/493D.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <cstring>
+#include <set>
+#include <queue>
+#include <cctype>
+#include <map>
+#include <stack>
+#include<cstdlib>
+#include <sstream>
+#include <deque>
+#include<list>
+#include<time.h>
+#define ll long long
+#define LL long long
+#define inf 1000000009
+#define iosfalse ios::sync_with_stdio(false);
+#define INF 1e8
+#define lowbit(x) x&-x
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+        {
+            cout<<"white"<<endl;
+            cout<<"1 2"<<endl;
+        }
+    else cout<<"black"<<endl;
+    return 0;
+}
