7e620401f6ef88655c5c20f73530f4dfee2d82a3
X <qq383441907@outlook.com>
2017-02-28 22:59:23 +0800

    KM 裸

---
+++ hdu/2255.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<cmath>
+#include<stack>
+#include<set>
+#include<vector>
+#include<map>
+#include<sstream>
+#define ll long long
+#define LL long long
+#define inf 1000000000
+#define iosfalse ios::sync_with_stdio(false);
+#define lowbit(x) x&-x
+using namespace std;
+
+
+const int maxn = 301;
+const int INF = (1<<31)-1;
+int w[maxn][maxn];
+int lx[maxn],ly[maxn]; //顶标
+int linky[maxn];
+int visx[maxn],visy[maxn];
+int slack[maxn];
+int nx,ny;
+bool find(int x)
+{
+    visx[x] = true;
+    for(int y = 0; y < ny; y++)
+    {
+        if(visy[y])
+            continue;
+        int t = lx[x] + ly[y] - w[x][y];
+        if(t==0)
+        {
+            visy[y] = true;
+            if(linky[y]==-1 || find(linky[y]))
+            {
+                linky[y] = x;
+                return true;        //找到增广轨
+            }
+        }
+        else if(slack[y] > t)
+            slack[y] = t;
+    }
+    return false;                   //没有找到增广轨（说明顶点x没有对应的匹配，与完备匹配(相等子图的完备匹配)不符）
+}
+
+int KM()                //返回最优匹配的值
+{
+    int i,j;
+    memset(linky,-1,sizeof(linky));
+    memset(ly,0,sizeof(ly));
+    for(i = 0; i < nx; i++)
+        for(j = 0,lx[i] = -INF; j < ny; j++)
+            if(w[i][j] > lx[i])
+                lx[i] = w[i][j];
+    for(int x = 0; x < nx; x++)
+    {
+        for(i = 0; i < ny; i++)
+            slack[i] = INF;
+        while(true)
+        {
+            memset(visx,0,sizeof(visx));
+            memset(visy,0,sizeof(visy));
+            if(find(x))                     //找到增广轨，退出
+                break;
+            int d = INF;
+            for(i = 0; i < ny; i++)          //没找到，对l做调整(这会增加相等子图的边)，重新找
+            {
+                if(!visy[i] && d > slack[i])
+                    d = slack[i];
+            }
+            for(i = 0; i < nx; i++)
+            {
+                if(visx[i])
+                    lx[i] -= d;
+            }
+            for(i = 0; i < ny; i++)
+            {
+                if(visy[i])
+                     ly[i] += d;
+                else
+                     slack[i] -= d;
+            }
+        }
+    }
+    int result = 0;
+    for(i = 0; i < ny; i++)
+    if(linky[i]>-1)
+        result += w[linky[i]][i];
+    return result;
+}
+int main()
+{
+    int n;
+    while(scanf("%d",&n)!=EOF)
+    {
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                scanf("%d",&w[i][j]);
+        nx=n;ny=n;
+        int ans=KM();
+        printf("%d\n",ans);
+    }
+    return 0;
+}
